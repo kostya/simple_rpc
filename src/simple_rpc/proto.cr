c7ade45912efa40570d2440b8fecb3a261ea3435
Konstantin Makarchev <kostya27@gmail.com>
2018-11-18 16:27:40 +0300

    allow only public methods

--- src/simple_rpc/proto.cr
+++ src/simple_rpc/proto.cr
@@ -8,39 +8,39 @@ module SimpleRpc::Proto
         def handle_http(path, raw, response)
           \{% begin %}
           case path
-
-          # todo; select only public methods
           \{% for m in @type.methods %}
-            when "/rpc_\{{m.name}}"
-              \{% if m.args.size > 0 %}
-                tuple = Tuple(\{{ m.args.map do |arg|
-                                if arg.restriction
-                                  arg.restriction
-                                else
-                                  raise "argument '#{arg}' in method '#{m.name}' must have a type restriction"
-                                end
-                              end.join(", ").id
-                              }})
+            \{% if m.visibility.stringify == ":public" %}
+              when "/rpc_\{{m.name}}"
+                \{% if m.args.size > 0 %}
+                  tuple = Tuple(\{{ m.args.map do |arg|
+                                  if arg.restriction
+                                    arg.restriction
+                                  else
+                                    raise "argument '#{arg}' in method '#{m.name}' must have a type restriction"
+                                  end
+                                end.join(", ").id
+                                }})
 
-                req = begin
-                  tuple.from_msgpack(raw)
-                rescue MessagePack::Error
-                  {SimpleRpc::Error::ERROR_UNPACK_REQUEST, "msgpack not matched with #{tuple.inspect}", nil}.to_msgpack(response)
+                  req = begin
+                    tuple.from_msgpack(raw)
+                  rescue MessagePack::Error
+                    {SimpleRpc::Error::ERROR_UNPACK_REQUEST, "msgpack not matched with #{tuple.inspect}", nil}.to_msgpack(response)
+                    return
+                  end
+                \{% else %}
+                  req = Tuple.new
+                \{% end %}
+
+                proto = \{{@type}}.new
+                res = begin
+                  proto.\{{m.name}}(*req)
+                rescue ex
+                  {SimpleRpc::Error::TASK_EXCEPTION, ex.message, nil}.to_msgpack(response)
                   return
                 end
-              \{% else %}
-                req = Tuple.new
-              \{% end %}
 
-              proto = \{{@type}}.new
-              res = begin
-                proto.\{{m.name}}(*req)
-              rescue ex
-                {SimpleRpc::Error::TASK_EXCEPTION, ex.message, nil}.to_msgpack(response)
-                return
-              end
-
-              {SimpleRpc::Error::OK, nil, res}.to_msgpack(response)
+                {SimpleRpc::Error::OK, nil, res}.to_msgpack(response)
+            \{% end %}
           \{% end %}
 
           else
@@ -51,19 +51,20 @@ module SimpleRpc::Proto
       end
 
       class Client < SimpleRpc::Client
-        # todo; select only public methods
         \{% for m in @type.methods %}
-          \{% if !m.return_type %}
-            \{% raise "method '#{m.name}' must have a return type" %}
+          \{% if m.visibility.stringify == ":public" %}
+            \{% if !m.return_type %}
+              \{% raise "method '#{m.name}' must have a return type" %}
+            \{% end %}
+
+            \{% args_list = m.args.join(", ").id %}
+            \{% args = m.args.map { |a| a.name }.join(", ").id %}
+
+            def \{{m.name}}(\{{args_list}})
+              resp = @raw_client.request(\{{m.name.stringify}}, Tuple.new(\{{args.id}}))
+              SimpleRpc::Result(\{{m.return_type.id}}).from(resp)
+            end
           \{% end %}
-
-          \{% args_list = m.args.join(", ").id %}
-          \{% args = m.args.map { |a| a.name }.join(", ").id %}
-
-          def \{{m.name}}(\{{args_list}})
-            resp = @raw_client.request(\{{m.name.stringify}}, Tuple.new(\{{args.id}}))
-            SimpleRpc::Result(\{{m.return_type.id}}).from(resp)
-          end
         \{% end %}
       end
     end
