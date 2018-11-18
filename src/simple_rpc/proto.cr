23d871f3e467a35c2fff4a087f6ae59ca08ac7d2
Konstantin Makarchev <kostya27@gmail.com>
2018-11-18 16:19:53 +0300

    refactor server part

--- src/simple_rpc/proto.cr
+++ src/simple_rpc/proto.cr
@@ -3,57 +3,53 @@ require "msgpack"
 
 module SimpleRpc::Proto
   macro included
-    def self.handle_http(path, raw, response)
-      \{% begin %}
-      case path
+    macro finished
+      class Server < SimpleRpc::Server
+        def handle_http(path, raw, response)
+          \{% begin %}
+          case path
 
-      # todo; select only public methods
-      \{% for m in @type.methods %}
-        when "/rpc_\{{m.name}}"
-          \{% if m.args.size > 0 %}
-            tuple = Tuple(\{{ m.args.map do |arg|
-                            if arg.restriction
-                              arg.restriction
-                            else
-                              raise "argument '#{arg}' in method '#{m.name}' must have a type restriction"
-                            end
-                          end.join(", ").id
-                          }})
+          # todo; select only public methods
+          \{% for m in @type.methods %}
+            when "/rpc_\{{m.name}}"
+              \{% if m.args.size > 0 %}
+                tuple = Tuple(\{{ m.args.map do |arg|
+                                if arg.restriction
+                                  arg.restriction
+                                else
+                                  raise "argument '#{arg}' in method '#{m.name}' must have a type restriction"
+                                end
+                              end.join(", ").id
+                              }})
 
-            req = begin
-              tuple.from_msgpack(raw)
-            rescue MessagePack::Error
-              {SimpleRpc::Error::ERROR_UNPACK_REQUEST, "msgpack not matched with #{tuple.inspect}", nil}.to_msgpack(response)
-              return
-            end
-          \{% else %}
-            req = Tuple.new
+                req = begin
+                  tuple.from_msgpack(raw)
+                rescue MessagePack::Error
+                  {SimpleRpc::Error::ERROR_UNPACK_REQUEST, "msgpack not matched with #{tuple.inspect}", nil}.to_msgpack(response)
+                  return
+                end
+              \{% else %}
+                req = Tuple.new
+              \{% end %}
+
+              proto = \{{@type}}.new
+              res = begin
+                proto.\{{m.name}}(*req)
+              rescue ex
+                {SimpleRpc::Error::TASK_EXCEPTION, ex.message, nil}.to_msgpack(response)
+                return
+              end
+
+              {SimpleRpc::Error::OK, nil, res}.to_msgpack(response)
           \{% end %}
 
-          proto = self.new
-          res = begin
-            proto.\{{m.name}}(*req)
-          rescue ex
-            {SimpleRpc::Error::TASK_EXCEPTION, ex.message, nil}.to_msgpack(response)
-            return
+          else
+            {SimpleRpc::Error::UNKNOWN_METHOD, path, nil}.to_msgpack(response)
           end
-
-          {SimpleRpc::Error::OK, nil, res}.to_msgpack(response)
-      \{% end %}
-
-      else
-        {SimpleRpc::Error::UNKNOWN_METHOD, path, nil}.to_msgpack(response)
+          \{% end %}
+        end
       end
-      \{% end %}
-    end
 
-    class Server < SimpleRpc::Server
-      def handle_http(path, raw, response)
-        {{@type}}.handle_http(path, raw, response)
-      end
-    end
-
-    macro finished
       class Client < SimpleRpc::Client
         # todo; select only public methods
         \{% for m in @type.methods %}
