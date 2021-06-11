f993b94cf6ac7af9fbc8b579644db75898ea33f6
Konstantin Makarchev <kostya27@gmail.com>
2021-06-11 22:42:19 +0300

    remote rb

--- rb/build.sh
+++
@@ -1,5 +0,0 @@
-cd ../
-crystal build rb/serv.cr --release -o rb/bin_serv
-crystal build rb/cli.cr --release -o rb/bin_cli
-rm *.dwarf
-cd -
--- rb/cli.rb
+++
@@ -1,6 +0,0 @@
-require 'msgpack/rpc'
-
-PORT = (ARGV[0] || 18800).to_i
-client = MessagePack::RPC::Client.new('127.0.0.1', PORT)
-result = client.call(:methodName, 1, 1.5, "2.7", {5 => 15.8})
-p result
